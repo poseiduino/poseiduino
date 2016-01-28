217ad1f586050b818f663d533b268da74539a30e
Admin poseiduino <poseiduino@example.com>
2016-01-28 06:29:32 +0100

    Uno: set to raw data input

--- raspi_uno/brightness/brightness.ino
+++ raspi_uno/brightness/brightness.ino
@@ -1,33 +1,14 @@
-const int numReadings = 10;
-unsigned int readings[numReadings];
-int index = 0;
-unsigned int total = 0;
-int average = 0;  
 
-int adc_AVG(byte channel)
-{
-    total= total - readings[index];        
-    readings[index] = analogRead(channel);
-    total= total + readings[index];      
-    index = index + 1;                    
-
-    if (index >= numReadings)index = 0;                          
-
-    average = total / numReadings;        
-    return average / 1024.0 * 100.0;
-}
+int analogPin = 0;
+int raw = 0;
 
 void setup()
 {
-
-    analogWrite(9,200); 
     Serial.begin(9600);
 }
 
 void loop()
 {
-
-    int raw_adc = adc_AVG(0);
-    Serial.println(100 - raw_adc);  
-    delay(20);
+    raw = 1023 - analogRead(0);
+    Serial.println(raw);  
 }
