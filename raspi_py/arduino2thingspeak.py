1051963372a19330c02ea851d52a341cadc3da4f
Admin poseiduino <poseiduino@example.com>
2016-01-29 15:20:34 +0100

    Python: added exception-handling

--- raspi_py/arduino2thingspeak.py
+++ raspi_py/arduino2thingspeak.py
@@ -7,7 +7,7 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 currTime = int(time.time())
 data = []
 
-def doit(f1val):
+def sendToThingspeak(f1val):
     print f1val
     params = urllib.urlencode({'field3': f1val, 'api_key':'YOURAPIKEY'})
     headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
@@ -19,10 +19,18 @@ def doit(f1val):
     conn.close()
  
 if __name__ == "__main__":
-    while True:
-        while (int(time.time()) - currTime  < 60):
-            data.append(int(ser.readline()))
-        brightnessVal = sum(data) / len(data)
-        data = []
-        currTime = int(time.time())
-        doit(brightnessVal)
+    print("Ctrl+c terminates programm")
+    print "Start reading ..."
+    try:
+        while True:
+            while (int(time.time()) - currTime  < 60):
+                try:
+                    data.append(int(ser.readline()))
+                except ValueError:
+                    print "ValueError: I try again ..."
+            brightnessVal = sum(data) / len(data)
+            data = []
+            currTime = int(time.time())
+            sendToThingspeak(brightnessVal)
+    except KeyboardInterrupt:
+        ser.close
